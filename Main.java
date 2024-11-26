eac2e192a49d94293d53e1d161efa36cb98494fb
cpcsyang <frank.yang@cedarpark.org>
2024-11-25 20:10:10 -0800

    overload methods

--- Main.java
+++ Main.java
@@ -5,13 +5,10 @@ public class Main {
 
     public void start() {
         Point p1 = new Point(1, 1);
+        Point p2 = new Point(4, 5);
         System.out.println(p1);
-        Point p2 = new Point();
         System.out.println(p2);
-        Point p3 = new Point(1.5, 3.7);
-        System.out.println(p3);
-//        Point p2 = new Point(0, 0);
-//        p2.translate(3, 4);
-//        System.out.println(p1.distanceTo(p2.getX(), p2.getY()));
+        System.out.println(p1.distanceTo(p2.getX(), p2.getY()));
+        System.out.println(p1.distanceTo(p2));
     }
 }
--- Point.java
+++ Point.java
@@ -27,21 +27,25 @@ public class Point {
         return y;
     }
 
-    public void setX(int x) {
+    public void setX(double x) {
         this.x = x;
     }
 
-    public void setY(int y) {
+    public void setY(double y) {
         this.y = y;
     }
 
-    public void translate(int dx, int dy) {
+    public void translate(double dx, double dy) {
         x += dx;
         y += dy;
     }
 
-    public double distanceTo(int otherX, int otherY) {
+    public double distanceTo(double otherX, double otherY) {
         return Math.sqrt((x - otherX) * (x - otherX) + (y - otherY) * (y - otherY));
+    }
+
+    public double distanceTo(Point other) {
+        return distanceTo(other.getX(), other.getY());
     }
 
     public String toString() {
